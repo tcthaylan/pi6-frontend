b8afc31aecb9901cf49f5f98612051701dbdae7a
Thaylan <thaylan.conceicao@nextpage.com.br>
2019-09-17 17:05:59 -0300

    front menu

--- src/routes/Menu/List/Index.jsx
+++ src/routes/Menu/List/Index.jsx
@@ -7,7 +7,13 @@ const MenuList = () => {
     <div className="menuContainer">
       <div className="banner-menu"/>
       <div className="menu">
-        <h2>Lanches Dolor</h2>
+        <h2 className="menu-title">Lanches Dolor</h2>
+        <div className="menu-filters">
+          <h4>Filtrar por</h4>
+          <button>Entrega</button>
+          <button>Cozinha</button>
+          <button>Pagamento</button>
+        </div>
         <ol className="menu-list">
           <li className="menu-list-item">
             <MenuItem
@@ -49,6 +55,49 @@ const MenuList = () => {
               image="hamburguer.jpg"
             />
           </li>
+          <li className="menu-list-item">
+            <MenuItem
+              name="Hamburguer médio"
+              price="24,99"
+              desc="Ex fugiat ut amet ipsum minim."
+              image="hamburguer.jpg"
+            />
+          </li>
+          <li className="menu-list-item">
+            <MenuItem
+              name="Hamburguer médio"
+              price="24,99"
+              desc="Ex fugiat ut amet ipsum minim."
+              image="hamburguer.jpg"
+            />
+          </li>
+          <li className="menu-list-item">
+            <MenuItem
+              name="Hamburguer médio"
+              price="24,99"
+              desc="Ex fugiat ut amet ipsum minim."
+              image="hamburguer.jpg"
+            />
+          </li>
+          <li className="menu-list-item">
+            <MenuItem
+              name="Hamburguer médio"
+              price="24,99"
+              desc="Ex fugiat ut amet ipsum minim."
+              image="hamburguer.jpg"
+            />
+          </li>
+        </ol>
+      </div>
+      <div className="paginate">
+        <ol className="paginate-list">
+          <li>{'<'}</li>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+          <li>4</li>
+          <li>5</li>
+          <li>{'>'}</li>
         </ol>
       </div>
     </div>

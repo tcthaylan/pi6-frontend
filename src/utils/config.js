28002c4697cbec45d7d017a6d4b32a7ccc57ae0a
Thaylan Conceição <tc.thaylan@gmail.com>
2019-11-12 06:22:08 -0300

    teste heroku

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,2 +1,4 @@
 
-export const ip = 'https://tranquil-castle-50490.herokuapp.com'
+export const ip = 'https://glacial-badlands-93377.herokuapp.com'
+// export const ip = 'https://tranquil-castle-50490.herokuapp.com'
+// export const ip = 'http://localhost:3000'
